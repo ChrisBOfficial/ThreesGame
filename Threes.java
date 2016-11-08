ac107cba3a6d7edb90cfcbd504dc14acd72a1123
Chris Burbach <officialchrisb@icloud.com>
2016-11-08 14:01:01 -0600

    Updated comments
    Clarified the first comment about how it works

--- Threes.java
+++ Threes.java
@@ -1,5 +1,5 @@
 /*
-First, you mash in a random large number to start with. Then, repeatedly do the following:
+First, you put in a random number to start with. Then, repeatedly do the following:
 If the number is divisible by 3, divide it by 3.
 If it's not, either add 1 or subtract 1 (to make it divisible by 3), then divide it by 3.
 The game stops when you reach "1".
